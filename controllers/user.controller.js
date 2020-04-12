6b0925a96d003f23b3e545119b19924397dcd227
Tunmbi Banto <hellotunmbi@gmail.com>
2020-04-12 16:39:59 +0100

    fixed sorting bug

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -14,7 +14,9 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.history = asyncHandler(async (req, res, next) => {
   const { id, phone } = req;
-  const history = await History.find({ userid: id, phone })sort({ transactionDate: -1 });
+  const history = await History.find({ userid: id, phone }).sort({
+    created_at: -1,
+  });
 
   if (history.length === 0) {
     res.json({
