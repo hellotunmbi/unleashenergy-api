fac7b13f0ad9683a4c52d29d441982bdd457bcc3
Tunmbi Banto <hellotunmbi@gmail.com>
2020-04-16 18:29:46 +0100

    new resend otp route

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -24,8 +24,8 @@ exports.login = asyncHandler(async (req, res, next) => {
     res.json({
       status: 400,
       data: {
-        message: "phone number required"
-      }
+        message: "phone number required",
+      },
     });
     return;
   }
@@ -43,7 +43,7 @@ exports.login = asyncHandler(async (req, res, next) => {
       phone: phone,
       status: "pending",
       role: "user",
-      authCode: otp
+      authCode: otp,
     });
 
     sendOTPSMS(phone, otp);
@@ -52,8 +52,8 @@ exports.login = asyncHandler(async (req, res, next) => {
       status: 200,
       data: {
         message: "User registered",
-        phone
-      }
+        phone,
+      },
       // sms: smsSent.body
     });
   } else {
@@ -69,8 +69,8 @@ exports.login = asyncHandler(async (req, res, next) => {
       status: 200,
       data: {
         message: "User found but needs to login",
-        phone
-      }
+        phone,
+      },
     });
     // }
   }
@@ -86,8 +86,8 @@ exports.verifyOTP = asyncHandler(async (req, res, next) => {
     res.json({
       status: 400,
       data: {
-        message: "Phone Number and OTP required"
-      }
+        message: "Phone Number and OTP required",
+      },
     });
     return;
   }
@@ -101,8 +101,8 @@ exports.verifyOTP = asyncHandler(async (req, res, next) => {
     res.json({
       status: 400,
       data: {
-        message: "Invalid OTP"
-      }
+        message: "Invalid OTP",
+      },
     });
   } else {
     if (verifiedOTP["status"] && verifiedOTP["status"] != "active") {
@@ -110,15 +110,15 @@ exports.verifyOTP = asyncHandler(async (req, res, next) => {
         status: 200,
         data: {
           message: "OTP Verified but user not active yet",
-          status: verifiedOTP["status"]
-        }
+          status: verifiedOTP["status"],
+        },
       });
       return;
     } else if (verifiedOTP["status"] && verifiedOTP["status"] === "active") {
       const token = jwt.sign(
         {
           id: verifiedOTP._id,
-          phone
+          phone,
         },
         process.env.JWT_SECRET,
         { expiresIn: "1y" }
@@ -128,15 +128,15 @@ exports.verifyOTP = asyncHandler(async (req, res, next) => {
         data: {
           message: "User found. You can login",
           user: verifiedOTP,
-          token
-        }
+          token,
+        },
       });
     } else {
       res.json({
         status: 400,
         data: {
-          message: "Unable to verify OTP"
-        }
+          message: "Unable to verify OTP",
+        },
       });
     }
   }
@@ -151,8 +151,8 @@ exports.register = asyncHandler(async (req, res, next) => {
     res.json({
       status: 400,
       data: {
-        message: "Full Name and Email Address are required"
-      }
+        message: "Full Name and Email Address are required",
+      },
     });
   }
 
@@ -165,7 +165,7 @@ exports.register = asyncHandler(async (req, res, next) => {
   const token = jwt.sign(
     {
       id: registered._id,
-      phone
+      phone,
     },
     process.env.JWT_SECRET,
     { expiresIn: "1y" }
@@ -176,8 +176,8 @@ exports.register = asyncHandler(async (req, res, next) => {
     data: {
       message: "User successfully registered",
       user: registered,
-      token
-    }
+      token,
+    },
   });
 });
 
@@ -187,13 +187,28 @@ exports.register = asyncHandler(async (req, res, next) => {
 generateOTP = () => Math.floor(Math.random() * 9000);
 
 // ---------------------------------------------------------
+// Resend OTP...
+
+exports.resendOTP = asyncHandler(async (req, res, next) => {
+  const { phone, otp } = req.body;
+
+  if (!phone || !otp) {
+    return next(
+      new ErrorResponse("Expecting phone and otp but got incomplete data", 400)
+    );
+  }
+
+  sendOTPSMS(phone, otp);
+});
+
+// ---------------------------------------------------------
 // Send OTP SMS...
 sendOTPSMS = async (phone, otp) => {
   // Send OTP as SMS...
   var payload = {
     to: phone,
     from: "Unleash Energy",
-    message: `Unleash Energy OTP Code is ${otp}`
+    message: `Unleash Energy OTP Code is ${otp}`,
   };
 
   const smsSent = await jusibe.sendSMS(payload);
@@ -216,18 +231,18 @@ exports.sendEmail = (req, res) => {
     from: "Unleash Energy <info@unleashenergy.com>",
     subject: "Test Email with Unleash",
     text: "intro title header",
-    html: "This is a test email with Unleash Energy"
+    html: "This is a test email with Unleash Energy",
   };
   sgMail
     .send(msg)
-    .then(sent => {
+    .then((sent) => {
       res.json({
-        message: "Email Sent Successfully"
+        message: "Email Sent Successfully",
       });
     })
-    .catch(err =>
+    .catch((err) =>
       res.json({
-        message: "Unable to send message"
+        message: "Unable to send message",
       })
     );
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -12,6 +12,7 @@ router.post("/login", authController.login);
 router.post("/verifyotp", authController.verifyOTP);
 router.post("/register", authController.register);
 
+router.post("/resendOTP", authController.resendOTP);
 router.post("/sendemail", authController.sendEmail);
 
 module.exports = router;
